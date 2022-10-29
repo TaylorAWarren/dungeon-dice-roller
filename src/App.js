25ed849f874ad02b4de98b18234ea24cea849827
Jonathan Neuman <jonrileyneuman@gmail.com>
2022-10-29 00:37:06 -0600

    I changed nothing relevant

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h3> Jon and Taylor project </h3>
+        <h4>It doesn't have to be good - (Taylor Warren, October 2022)</h4>
+        <h5>bruh</h5>
       </header>
     </div>
   );
